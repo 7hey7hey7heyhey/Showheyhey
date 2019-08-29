3c87a08ddab6c8143213f91154188dfd37a83dde
7hey7hey7heyhey <54656666+7hey7hey7heyhey@users.noreply.github.com>
2019-08-29 13:58:11 +0900

    Add files via upload

---
+++ a0010.py
@@ -0,0 +1,21 @@
+
+
+
+
+import glob
+File_LIST = glob.glob("//cksvf3/00公開/10102500千葉プロセスグループ/700 パイロット各種情報交換用/押出機技術資料/*")
+
+
+
+A = "C:\\Users\\u00180118\\Desktop\\python code"
+B = "C:\\Users\\u00180118\\Desktop\\python code\\a0006.py"
+import os
+os.path.isfile(File_LIST[1])
+os.path.isfile(B)
+os.path.isfile(A)
+
+# https://hydrocul.github.io/wiki/programming_languages_diff/io/file-type.html
+
+
+
+
