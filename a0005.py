cb1a6fe8b6a643b902b2543be3cca860e019bc5e
7hey7hey7heyhey <54656666+7hey7hey7heyhey@users.noreply.github.com>
2019-08-29 13:59:26 +0900

    Add files via upload

---
+++ a0005.py
@@ -0,0 +1,16 @@
+# https://note.nkmk.me/python-xlrd-xlwt-usage/
+
+'''
+$ pip3 install xlrd
+pip list
+
+'''
+import matplotlib
+a = [1, 2, 3, 4, 5]
+for aa in a:
+    print(aa)
+
+
+import numpy
+
+# https://qiita.com/HiroCh/items/481adfa969dbe689f566
