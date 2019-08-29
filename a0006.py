eed87112283a26dcd4bd4a7d4cbaee59f6b4e7a6
7hey7hey7heyhey <54656666+7hey7hey7heyhey@users.noreply.github.com>
2019-08-29 13:59:13 +0900

    Add files via upload

---
+++ a0006.py
@@ -0,0 +1,58 @@
+
+
+
+import os 
+os.getcwd() # 現在のディレクトリを表示
+os.chdir("C:\\Users\\u00180118\\Desktop\\python code") # ディレクトリの変更
+
+Dir_set1 = "C:\\Users\\u00180118\\Desktop" # 検索したいフォルダ（ディレクトリを設定する。）
+Dir_set2 = "C:\\Users\\u00180118\\Desktop\\python code" # 検索したいフォルダ（ディレクトリを設定する。）
+
+os.chdir(Dir_set2) # ディレクトリの変更
+
+##
+
+# フォルダからファイル一覧を取得する。
+
+import pathlib
+
+path = pathlib.Path('.') # カレントフォルダ('.')を指定
+
+file_list1 = [] # 空のリストを作る。
+for po in path.iterdir(): # •ディレクトリ内のファイル・サブディレクトリ一覧を取得 iterdir
+    # print(type(path.iterdir()))
+    if not po.is_dir(): # フォルダでない場合(つまり、何らかのファイルの場合（例えば、エクセルファイルとかWordファイルのとき）)に表示する。
+        file_list1.append(po) # po をリストに追加する（poとは何らかのファイル）。
+        # print(type(po.is_dir()))
+        # print(po)
+
+print(file_list1) # ディレクトリ内のファイルを確認する。
+# file_list1[0]
+# file_list1[1]
+
+"""
+for ll in file_list1:
+    print(ll)
+"""
+
+#####
+kensaku = 'このショートカットに' # 検索したい言葉をここに入れる。
+
+file_name_list = [] # 空のリストを作る。
+for ll in file_list1: # ディレクトリ内のファイルひとつひとつをループ計算させる。
+    
+    text = [] # 空のリストを作る。
+    with open(ll,encoding='utf-8') as f: # 検索したいファイルを設定    
+        for line in f:
+            text.append(line)
+            # print(line)
+
+        # print(text), type(text)
+
+        list = text
+        for l in list:
+            if kensaku in l:
+                # type(l)
+                file_name_list.append(ll)
+
+ print(file_name_list)
